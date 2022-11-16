a5a34994a2d79bb0054e21f97129e4b8cab41d9f
anuragmirrorleech <115845369+anuragmirrorleech@users.noreply.github.com>
2022-11-16 10:10:38 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -56,7 +56,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/BikashTG/UniqueMusicBot",
+    "https://github.com/anuragmirrorleech/aaaa",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
